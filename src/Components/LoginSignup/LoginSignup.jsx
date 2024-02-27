78fa632f020e74dd606b95ea885d320786cc8e4e
Alex Situ <situalex123@gmail.com>
2024-02-26 18:25:27 -0800

    Finished login form front end

--- src/Components/LoginSignup/LoginSignup.jsx
+++ src/Components/LoginSignup/LoginSignup.jsx
@@ -1,35 +1,43 @@
-import React from 'react'
+import React,{ useEffect, useState } from 'react'
 import './LoginSignup.css'
 import user_icon from '../Assets/person.png'
 import email_icon from '../Assets/email.png'
 import password_icon from '../Assets/password.png'
 
-const ICON_SIZE = 20;
 const LoginSignup = () => {
+    
+    const [action, setAction] = useState("Login")
+
   return (
     <div className = 'container'>
         <div className="header">
-            <div className="text">Sign Up</div>
+            <div className="text">{action}</div>
             <div className="underline"></div>
         </div>
     <div className="inputs">
+        {action==="Sign Up"? 
         <div className="input">
-            <img src={user_icon} alt="" width={ICON_SIZE} height={ICON_SIZE}/>
-            <input type="text" />
+            <img src={user_icon} alt=""/>
+            <input type="text" placeholder="Name" />
+        </div>
+        :<div></div>}
+        <div className="input">
+            <img src={email_icon} alt=""/>
+            <input type="email" placeholder="Email" />
         </div>
         <div className="input">
-            <img src={email_icon} alt="" width={ICON_SIZE} height={ICON_SIZE}/>
-            <input type="email" />
-        </div>
-        <div className="input">
-            <img src={password_icon} alt="" width={ICON_SIZE} height={ICON_SIZE}/>
-            <input type="password" />
+            <img src={password_icon} alt=""/>
+            <input type="password" placeholder='Password'/>
         </div>
     </div>
-    <div className="forgot-password"><span>Lost Password?</span></div>
+
+        {action==="Login" && <div className="option"><span>Lost Password?</span></div>}
+        {action=== "Login" && <div className="option"><span onClick={()=>{setAction("Sign Up")}}>Dont' have an account?</span></div>}
+        {action==="Sign Up" && <div className="option"><span onClick={()=>{setAction("Login")}}>Already have an account?</span></div>}
+
+   
     <div className="submit-container">
-        <div className="submit">Sign Up</div>
-        <div className="submit">Login</div>
+        {action==="Sign Up"?<div className="submit">Sign Up</div> : <div className="submit">Login</div>}
     </div>
     </div>
   )
